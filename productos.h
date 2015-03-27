314305b626a8dc2cb647585940dfb96c0739f9e5
Ambar Suarez <ambarsuarez@unitec.edu>
2015-03-27 15:05:43 -0600

    agregar funcion

---
+++ productos.h
@@ -0,0 +1,25 @@
+//Ambar Suarez Adriana Vega Oscar Carranza
+#ifndef PRODUCTOS_H
+#define PRODUCTOS_H
+#include <string>
+#include <Cliente.h>
+
+using std::string;
+
+class Productos{
+	string nombre;
+	double volumen;
+	double peso;
+	double precio;
+	Cliente cliente;
+public:
+	Productos(string,double,double,double,Cliente);
+	virtual string toString()const;
+	virtual double CobrarImpuesto()=0;
+	string getNombre()const;
+	double getVolumen()const;
+	double getPeso()const;
+	double getPrecio()const;
+	Cliente* getCliente()const;
+};
+#endif
